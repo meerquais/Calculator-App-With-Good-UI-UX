ff8e21f846d00e4b03ebf96e316e9a7b1043c29c
Meer Quais <meerquais017@gmail.com>
2023-05-13 02:10:29 +0500

    fixed NaN and Infinity

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ function setNumber(num){
     input.value += num
 }
 
+function result() {
+    var inputValue = document.getElementById("number");
+    try {
+      var output = eval(inputValue.value);
+      if (output === Infinity || output === -Infinity || isNaN(output)) {
+        throw new Error("Error");
+      }
+      inputValue.value = output;
+    } catch (err) {
+      inputValue.value = "Error";
+    }
+  }
+  
 
-
-function result(){
-    var inputValue = document.getElementById("number")
-    var output = eval(inputValue.value)
-    inputValue.value = output
-}
 
 function clearScreen() {
     var inputValue = document.getElementById("number")
